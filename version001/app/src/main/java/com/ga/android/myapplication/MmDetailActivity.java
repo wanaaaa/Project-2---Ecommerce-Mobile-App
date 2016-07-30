23be358e1e9fd75a9d13ec708652a3df21535068
wanaaaa <wanaaaa@gmail.com>
2016-07-30 17:12:47 -0400

    image

--- version001/app/src/main/java/com/ga/android/myapplication/MmDetailActivity.java
+++ version001/app/src/main/java/com/ga/android/myapplication/MmDetailActivity.java
@@ -3,6 +3,7 @@ package com.ga.android.myapplication;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,11 +13,21 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.squareup.picasso.Picasso;
+
 import java.net.URL;
 
 public class MmDetailActivity extends AppCompatActivity {
     TextView TVtitle, TVprice;
     ImageView image;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +41,12 @@ public class MmDetailActivity extends AppCompatActivity {
         String price = intent.getStringExtra("price");
 
         image = (ImageView) findViewById(R.id.detailImage);
+        String imgURL = "http://productshots1.modcloth.net/productshots/0169/8189/3af49d5c38618db1d24e5a460feea5ba.jpg";
+        String imgFTP = "file:///documents_wan/image/dress1.jpg";
+        String webHard = "http://i61.photobucket.com/albums/h53/wanaaaa1/dress1_zps0f7ysrew.jpg";
+        Picasso.with(this)
+                .load(webHard).resize(300, 400).centerCrop()
+                .into(image);
 //        URL url = new URL("http://g.nordstromimage.com/ImageGallery/store/product/Zoom/2/10945202.jpg");
 //        Bitmap bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream());
 //        image.setImageBitmap(bmp);
@@ -48,6 +65,48 @@ public class MmDetailActivity extends AppCompatActivity {
 //                        .setAction("Action", null).show();
 //            }
 //        });
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "MmDetail Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.ga.android.myapplication/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "MmDetail Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.ga.android.myapplication/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
 }
